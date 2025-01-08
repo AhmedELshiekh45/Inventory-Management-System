237271e4c7bd3a767a9667a9f00e44674d49bef4
Ahmed ELshiekh <ahmedelshiekh190@gmail.com>
2025-01-08 18:30:54 +0200

    Add project files.

---
+++ Domain/Base/BaseClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModels.Base
+{
+    public class BaseClass
+    {
+        public string Id { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
